cb9131397e3d18997d9f396d448d2221d15eaac8
jambrizz <ambrizjovani@gmail.com>
2023-01-12 00:25:54 -0800

    prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,45 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-
-        Random randomGenerator = new Random();
-        int num = randomGenerator.Next(1, 101);
-
-        Console.Write("What is your guess? ");
-        int guess = Convert.ToInt32(Console.ReadLine());
-
-        while (guess != num)
+        string answer;
+        do
         {
-            if (guess > num)
-            {
-                Console.WriteLine("Your guess was too high.");
-            }
-            else
-            {
-                Console.WriteLine("Your guess was too low.");
-            }
-
+            Console.WriteLine("Hello Prep3 World!");
+    
+            Random randomGenerator = new Random();
+            int num = randomGenerator.Next(1, 101);
+            Console.WriteLine("I'm thinking of a number between 1 and 100. Can you guess it?");
             Console.Write("What is your guess? ");
-            guess = Convert.ToInt32(Console.ReadLine());
-        }
-        if (guess == num)
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+
+            while (guess != num)
+            {
+                if (guess > num)
+                {
+                    Console.WriteLine("Your guess was too high.");
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine("Your guess was too low.");
+                    count++;
+                }
+
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+            if (guess == num)
+            {
+                Console.WriteLine("You guessed correctly!");
+                count++;
+            }
+            Console.WriteLine($"It took you {count} guesses.");
+            Console.Write("Would you like to play again? (yes/no) ");
+            answer = Console.ReadLine();
+        } 
+        while (answer == "yes");
+        if (answer == "no")
         {
-            Console.WriteLine("You guessed correctly!");
+            Console.WriteLine("Thank you for playing, goodbye!");
         }
-        /*
-        if (guess == num)
-        {
-            Console.WriteLine("You guessed correctly!");
-        }
-        else if (guess > num)
-        {
-            Console.WriteLine("Your guess was too high.");
-        }
-        else
-        {
-            Console.WriteLine("Your guess was too low.");
-        }
-        */
     }
 }

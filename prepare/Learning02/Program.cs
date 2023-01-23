091557bd65f739ae20f5f91c9578c2a36d0b0ff6
jambrizz <ambrizjovani@gmail.com>
2023-01-22 20:43:13 -0800

    Learning02 final draft

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
 
         // Create a new instance of the Job class
         Job job1 = new Job();
@@ -17,9 +17,49 @@ class Program
         job2._jobTitle = "Junior Software Engineer";
         job2._company = "Adobe";
         job2._startYear = 2020;
-        job2._endYear = 2024;
+        job2._endYear = 2023;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Senior Software Engineer";
+        job3._company = "Google";
+        job3._startYear = 2020;
+        job3._endYear = 2022;
+
+        Job job4 = new Job();
+        job4._jobTitle = "Senior Software Engineer";
+        job4._company = "Apple";
+        job4._startYear = 2022;
+        job4._endYear = 2025;
+
+        Job job5 = new Job();
+        job5._jobTitle = "Jr. Software Engineer";
+        job5._company = "Amazon";
+        job5._startYear = 2023;
+        job5._endYear = 2025;
 
         //job1.DisplayJobDetails();
         //job2.DisplayJobDetails();
+
+        // Create a new instance of the Resume class
+        Resume resume = new Resume();
+        resume._name = "John Thompson";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job3);
+        resume._jobs.Add(job4);
+
+        Resume resume2 = new Resume();
+        resume2._name = "Peter Parker";
+        resume2._jobs.Add(job2);
+        resume2._jobs.Add(job5);
+
+        //Console.WriteLine(resume._jobs[0]._jobTitle);
+        //Console.WriteLine(resume2._jobs[0]._jobTitle);
+        //Console.WriteLine(resume._jobs.Count);
+        //Console.WriteLine(resume2._jobs.Count);
+        resume.DisplayResume();
+        Console.WriteLine();
+        resume2.DisplayResume();
+        Console.WriteLine();
+
     }
 }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -5,4 +5,15 @@ public class Resume
     public string _name;
 
     public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Resume for {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
 } 

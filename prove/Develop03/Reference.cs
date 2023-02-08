c8be97d817465919a468038da38e8c280f56bfca
jambrizz <ambrizjovani@gmail.com>
2023-02-08 00:52:50 -0800

    Scripture memorizer structure setup

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Reference
+{
+    private string _book;
+
+    private int _chapter;
+
+    private int _verse;
+
+    public Reference()
+    {
+
+    }
+
+    public Reference(int amountOfVerses)
+    {
+
+    }
+
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class Word
+{
+    private List<int> _savedIndexes = new List<int>();
+
+    public void Words()
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public string GetWord()
+    {
+        //TODO fix the return below
+        return "test";
+    }
+
+    public string GetHiddenWord()
+    {
+        //TODO fix the return below
+        return "test";
+    }
+
+    public string SetWord()
+    {
+        //TODO fix the return below
+        return "test";
+    }
+}

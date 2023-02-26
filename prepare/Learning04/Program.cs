94812fa625b95a14dd101e366ef2f06e18123d27
jambrizz <ambrizjovani@gmail.com>
2023-02-25 23:54:11 -0800

    Learning04 assignment complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,18 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        /*
+        Console.Clear();
         Assignment assignment1 = new Assignment();
         assignment1.SetStudentName("John");
         assignment1.SetTopic("Math");
-        Console.WriteLine(assignment1.GetSummary());
-        */
-        Console.Clear();
+        Console.WriteLine(assignment1.GetSummary() + "\n");
+        
+        //Console.Clear();
         MathAssignment Assignment2 = new MathAssignment();
         Assignment2.SetStudentName("Roberto Rodriguez");
         Assignment2.SetTopic("Algebra");
         Assignment2.SetMathAssignment("Chapter 2", "1-10");
         Console.WriteLine(Assignment2.GetSummary());
-        Console.WriteLine(Assignment2.GetHomeworkList());
+        Console.WriteLine(Assignment2.GetHomeworkList() + "\n");
+
+        //Console.Clear();
+        WritingAssignment assignment3 = new WritingAssignment();
+        assignment3.SetStudentName("Mary Waters");
+        assignment3.SetTopic("European History");
+        assignment3.SetWritingAssignment("The Causes of World War II");
+        Console.WriteLine(assignment3.GetWritingAssignment());
     }
 }
--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -4,8 +4,18 @@ public class WritingAssignment : Assignment
 {
     private string _title;
 
-    public string GetWritingIinformation()
+    public string GetWritingInformation()
     {
         return $"Title: {_title}";
+    }
+
+    public void SetWritingAssignment(string title)
+    {
+        _title = title;
+    }
+
+    public string GetWritingAssignment()
+    {
+        return $"{GetSummary()}\n{GetWritingInformation()}  {GetStudentName()}\n";
     }
 }

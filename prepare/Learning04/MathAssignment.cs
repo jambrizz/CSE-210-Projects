0646881135c294d143b51fc4abc67c845acf15b3
jambrizz <ambrizjovani@gmail.com>
2023-02-25 17:37:59 -0800

    Learning04 MathAssignments class created

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -2,5 +2,12 @@ using System;
 
 public class MathAssignment : Assignment
 {
-    
+    private string _textbookSection;
+
+    private string _problems;
+
+    public string GetHomeworkList()
+    {
+        return $"Textbook section: {_textbookSection} Problems: {_problems}";
+    }
 }

436396325abb26cd3b503636aacb757e78cd5f1b
jambrizz <ambrizjovani@gmail.com>
2023-03-04 01:23:15 -0800

    Shape class and square class updated draft

--- prepare/Learning05/Square.cs
+++ prepare/Learning05/Square.cs
@@ -1,2 +1,12 @@
 using System;
 using System.Collections.Generic;
+
+public class Square : Shape
+{
+    private double _side;
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}

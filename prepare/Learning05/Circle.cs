f36a195b957bf31ca030799ad317656466e51afb
jambrizz <ambrizjovani@gmail.com>
2023-03-04 16:33:02 -0800

    Testing of Square and Rectangle classes

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -5,6 +5,13 @@ public class Circle : Shape
 {
     private double _radius;
 
+    //Constructor for Circle
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    //Override GetArea method
     public override double GetArea()
     {
         return Math.PI * _radius * _radius;
--- prepare/Learning05/Rectangle.cs
+++ prepare/Learning05/Rectangle.cs
@@ -7,6 +7,14 @@ public class Rectangle : Shape
 
     private double _width;
 
+    //Constructor for Rectangle
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    //Override GetArea method
     public override double GetArea()
     {
         return _length * _width;

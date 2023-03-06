e59ea51d77409ab6a51a4bf2a8433f7d32248c00
jambrizz <ambrizjovani@gmail.com>
2023-03-06 00:19:13 -0800

    Class structure created

--- prove/Develop05/Eternal.cs
+++ prove/Develop05/Eternal.cs
@@ -3,5 +3,34 @@ using System.Collections.Generic;
 
 public class Eternal : Goal
 {
-    
+    private string _GoalItem;
+
+    private int points;
+
+    private bool Status;
+
+    public Eternal(int score) : base(score)
+    {
+        Status = false;
+    }
+
+    public override int CalculateScore()
+    {
+        return points;
+    }
+
+    public override string RecordEvent()
+    {
+        return GoalItem;
+    }
+
+    public override bool IsComplete()
+    {
+        return Status;
+    }
+
+    public override string NewGoal()
+    {
+        return "";
+    }
 }
--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -3,5 +3,54 @@ using System.Collections.Generic;
 
 public abstract class Goal 
 {
+    protected int _score;
+
+    protected string GoalItem;
+
+    List<string> goals = new List<string>();
+
+    public List<string> GetListGoals()
+    {
+        return goals;
+    }
+
+    public Goal(int score)
+    {
+
+    }
+
+    public int GetScore()
+    {
+        return _score;
+    }
+
+    public void SetScore(int score)
+    {
+        _score = score;
+    }
+
+    public virtual int CalculateScore()
+    {
+        return 0;
+    }
+
+
+    public virtual string RecordEvent()
+    {
+        return "";
+    }
+
+    public virtual bool IsComplete()
+    {
+        return true;
+    }
+
+    public void SetListGoals(string GoalItem)
+    {
+        goals.Add(GoalItem);
+    }
+
+    public abstract string NewGoal();
+
 
 }

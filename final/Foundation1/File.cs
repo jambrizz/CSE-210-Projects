e5176a1bd9d5ea3fc518ec259524604476db1b69
jambrizz <ambrizjovani@gmail.com>
2023-03-26 23:35:28 -0700

    Foundation 1 complete

--- final/Foundation1/File.cs
+++ final/Foundation1/File.cs
@@ -6,14 +6,68 @@ using System.Threading.Tasks;
 
 public class File
 {
-    public void SaveFile(string _fileName, List<string> list1, List<string> list2)
+    public string _fileName;
+    public void SaveFile(string fileName, List<string> list)
     {
-
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (var item in list)
+            {
+                outputFile.WriteLine(item);
+            }
+        }
     }
     
-    public void LoadFile(string _fileName)
+    public void SaveTwoFiles(string fileName1, string fileName2, List<string> list1, List<string> list2)
     {
+        using (StreamWriter outputFile = new StreamWriter(fileName1))
+        {
+            foreach (var item in list1)
+            {
+                outputFile.WriteLine(item);
+            }
+        }
 
+        using (StreamWriter outputFile = new StreamWriter(fileName2))
+        {
+            foreach (var item in list2)
+            {
+                outputFile.WriteLine(item);
+            }
+        }
+
+    }
+    public void LoadFile(string fileName, List<string> list)
+    {
+        using (StreamReader inputFile = new StreamReader(fileName))
+        {
+            string newLine;
+            while ((newLine = inputFile.ReadLine()) != null)
+            {
+                list.Add(newLine);
+            }
+        }
+    }
+
+    public void LoadTwoFiles(string fileName1, string fileName2, List<string> list1, List<string> list2)
+    {
+        using (StreamReader inputFile = new StreamReader(fileName1))
+        {
+            string newLine;
+            while ((newLine = inputFile.ReadLine()) != null)
+            {
+                list1.Add(newLine);
+            }
+        }
+
+        using (StreamReader inputFile = new StreamReader(fileName2))
+        {
+            string newLine;
+            while ((newLine = inputFile.ReadLine()) != null)
+            {
+                list2.Add(newLine);
+            }
+        }
     }
 
     public void DisplayListContents(List<string> list)
